70b7b071ddae57f7a7aef142bb26adeb8ab3bed9
Nouman Jamil <noumanjamil113@gmail.com>
2024-03-05 17:22:40 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+// ADDITION OPERATOR :
+var num1 = 45;
+var num2 = 55;
+var add = num1 + num2;
+console.log("Additon: ".concat(add));
+// SUBTRACTION OPERATOR :
+var a = 85;
+var b = 55;
+var sub = a - b;
+console.log("Subtraction: ".concat(sub));
+// MULTIPLICATION : 
+var c = 45;
+var d = 2;
+var Multiple = c * d;
+console.log("Multiplicaton: ".concat(Multiple));
+// DIVION :
+var e = 25;
+var f = 5;
+var Div = e / f;
+console.log("DIVION: ".concat(Div));
+// EXPONENTIATION :
+var Ball = 5;
+var Bat = 3;
+var Power = Math.pow(Ball, Bat);
+console.log("Exponentation: ".concat(Power));
+// MODULUS :
+var apple = 50;
+var bags = 8;
+var MODULUS = apple % bags;
+console.log("Modulus: ".concat(MODULUS));
---
+++ app.ts
@@ -0,0 +1,42 @@
+// ADDITION OPERATOR :
+let num1 = 45;
+let num2 = 55;
+
+let add = num1 + num2;
+console.log(`Additon: ${add}`);
+
+// SUBTRACTION OPERATOR :
+let a = 85;
+let b = 55;
+
+let sub = a - b;
+console.log(`Subtraction: ${sub}`);
+
+// MULTIPLICATION : 
+let c = 45;
+let d = 2;
+
+let Multiple = c * d;
+console.log(`Multiplicaton: ${Multiple}`);
+
+// DIVION :
+let e = 25;
+let f = 5;
+
+let Div = e / f;
+console.log(`DIVION: ${Div}`);
+
+// EXPONENTIATION :
+let Ball = 5;
+let Bat = 3;
+
+let Power = Ball**Bat;
+console.log(`Exponentation: ${Power}`);
+
+// MODULUS :
+let apple = 50;
+let bags = 8;
+
+let MODULUS = apple % bags;
+console.log(`Modulus: ${MODULUS}`);
+
